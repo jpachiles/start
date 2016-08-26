59401e7acf632f4c54d139617e5c64725f84069d
João Pedro Achiles <jpachiles@inforeason.com>
2016-08-26 13:46:54 -0300

    - Starting Mayhem

--- Learning/file.py
+++ Learning/file.py
@@ -1,4 +1,55 @@
 import os
-print 'Teste servidor'
 
-os.system('echo "asdasd"')
+
+
+"""
+os.system()
+import urllib2
+response = urllib2.urlopen('http://pythonforbeginners.com/')
+print response.info()
+html = response.read().replace('="http://', '\n')
+print html
+response.close()
+
+"""
+
+
+def welcome():
+    print ' __   __ ___  __  __ _   _  ___  __   __ '
+    print '|  \_/  | /\ |\ \/ /| |_| ||  _||  \_/  |'
+    print '|   _   | -- | |  | |  _  ||  _ |   _   |'
+    print '|__| |__|_||_| |__| |_| |_||___||__| |__|'
+
+def login():
+
+    login = None
+
+    while not login:
+        os.system('clear')
+        code = raw_input("Enter you password\n")
+        answer = code.encode('base64', 'strict').replace('\n', '')
+
+        if answer == 'aWRpb21h':
+            print 'Correct Login'
+            return True
+        else:
+            print 'Login Failed'
+
+def tests():
+    idioma = 'id1om4'
+    maca = 'm$c~~'
+
+    print idioma.encode('base64', 'strict').replace('\n', '')
+    print maca.encode('base64', 'strict').replace('\n', '')
+    code = raw_input("Enter you password\n")
+    print code.encode('base64', 'strict').replace('\n', '')
+
+def main():
+    if login:
+        welcome()
+    #sessao = login()
+    #tests()
+
+
+if __name__ == "__main__":
+    main()
